38db8f1df83b8108566b95dc48b79f6079ede259
DELL <DELL@DESKTOP-UNJEAPQ>
2026-01-27 11:08:39 +0530

    add mavenjdbc

--- git_test/src/git_test/Sample.java
+++ git_test/src/git_test/Sample.java
@@ -4,7 +4,7 @@ public class Sample {
 
 	public static void main(String[] args)
 	{
-		System.out.println("helo");
+		System.out.println("helo world");
 
 	}
 
---
+++ sampleApp/src/main/java/com/sample/sampleApp/App.java
@@ -0,0 +1,9 @@
+package com.sample.sampleApp;
+
+public class App 
+{
+    public static void main(String[] args) 
+    {
+        
+    }
+}
